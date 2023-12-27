598aeecb8a904dac816544b297a45a25f8606e60
markusz <markus_zhang@hotmail.com>
2023-12-26 21:58:51 -0500

    Created a basic interpreter

--- src/ch8/interpreter.py
+++ src/ch8/interpreter.py
@@ -8,7 +8,7 @@ class Token:
         self.lexeme = lexeme
 
 # Global Variables
-trace = True        # Controls token trace
+trace = False       # Controls token trace
 source = ''         # receives entire source program
 sourceindex = 0     # index into source
 line = 0            # current line number
@@ -17,6 +17,8 @@ tokenlist = []      # list of tokens to be consumed by parser
 tokenindex = 0
 prevchar = '\n'     # '\n' in prevchar signals start of new line
 blankline = True    # Set to False if line is not blank
+symboltable = {}    # Symbol Table for the interpreter
+operandstack = []          # Use a list for the stack
 
 # Category constants
 EOF                 = 0
@@ -275,17 +277,22 @@ def printstmt():
     # <printstmt>       -> 'print' '(' <expr> ')'
     # We already know the first token must be 'print' so no need to check
     advance()
-    consume(smalltokens['('])
-    expr()
-    consume(smalltokens[')'])
+    consume(LEFTPAREN)
+    expr()  # Expect expr() to push the result onto the top of the operand stack
+    print(operandstack.pop())
+    consume(RIGHTPAREN)
 
 def assignmentstmt():
     # <assignmentstmt>  -> NAME '=' <expr>
     # We already know the first token must be NAME so no need to check
-    # FIXME: Why doesn't the parser prepare a symbol table?
+    # Pick up the token as symbol
+    left = token.lexeme
     advance()
-    consume(smalltokens['='])
+    consume(ASSIGNOP)
     expr()
+    # expr() pushes onto top of the operand stack, update symbol table
+    global symboltable
+    symboltable[left] = operandstack.pop()
 
 def passstmt():
     advance()
@@ -298,16 +305,38 @@ def expr():
     # NOTE: We will be able to find one in term() (or its functions)
     term()
     while token.category == PLUS or token.category == MINUS:
+        # Now the left side was pushed onto the operand stack
+        # Note that the left side must be in the loop for multiple operations
+        optoken = token
+        global operandstack
+        left = operandstack.pop()
         advance()
         term()
+        # Now the right side was pushed onto the operand stack
+        right = operandstack.pop()
+        if optoken.category == PLUS:
+            operandstack.append(left + right)
+        else:
+            operandstack.append(left - right)
 
 def term():
     # <term>            -> <factor> ('*' <factor>)*
     # <term>            -> <factor> ('/' <factor>)*
     factor()
     while token.category == TIMES or token.category == DIVISION:
+        # Now the left side was pushed onto the operand stack
+        # Note that the left side must be in the loop for multiple operations
+        optoken = token
+        global operandstack
+        left = operandstack.pop()
         advance()
         factor()
+        # Now the right side was pushed onto the operand stack
+        right = operandstack.pop()
+        if optoken.category == TIMES:
+            operandstack.append(left * right)
+        else:
+            operandstack.append(left / right)
 
 def factor():
     # <factor>          -> '+' <factor>
@@ -315,17 +344,27 @@ def factor():
     # <factor>          -> NAME
     # <factor>          -> UNSIGNEDINT
     # <factor>          -> '(' <expr> ')'
-    # FIXME: Why doesn't factor() actually, say, do some calculations?
-    # FIXME: Actually where is the parser tree?
+    global operandstack, symboltable
     if token.category == PLUS:
         advance()
         factor()
+        # Now the right side must also be on the stack
+        right = operandstack.pop()
+        operandstack.append(right)
     elif token.category == MINUS:
         advance()
         factor()
+        # Now the right side must also be on the stack
+        right = operandstack.pop()
+        operandstack.append(-1 * right)
     elif token.category == NAME:
+        # NAME in <factor> is always on the right side, so check whether it exists
+        if token.lexeme not in symboltable:
+            raise RuntimeError(f"Name {token.lexeme} is not defined.")
+        operandstack.append(symboltable[token.lexeme])
         advance()
     elif token.category == UNSIGNEDINT:
+        operandstack.append(int(token.lexeme))
         advance()
     elif token.category == LEFTPAREN:
         advance()
@@ -352,10 +391,16 @@ def consume(expectedcat: int):
     """
     if token.category != expectedcat:
         raise RuntimeError(f"Expecting {catnames[expectedcat]} but get {catnames[token.category]}")
+    elif token.category == EOF:
+        # We are done
+        return
     else:
         advance()
 
 def parser():
+    global token, tokenindex
+    token = tokenlist[0]
+    tokenindex = 0
     program()
     print("End of parsing")
 
@@ -373,7 +418,7 @@ def main():
             sys.exit(1)
     else:
         print('Wrong number of command line arguments')
-        print('Usage: python3 tokenizer.py <infile>')
+        print('Usage: python3 interpreter.py <infile>')
         sys.exit(1)
 
     # Add newline to end if missing TODO: Why does the last line need a newline?
